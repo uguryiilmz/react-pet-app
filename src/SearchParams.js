f1f88bd3022dca1e3b0c5aef0d0d415e8708bc1d
uguryilmazunified <ugur@unified.com>
2020-06-19 17:29:37 -0400

    pet app

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 import Results from "./Results";
+import ThemeContext from './ThemeContext'
+
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA");
@@ -9,6 +11,7 @@ const SearchParams = () => {
   const [pets, setPets] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+  const [theme, setTheme]=useContext(ThemeContext)
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -17,9 +20,9 @@ const SearchParams = () => {
       type: animal
     });
 
-    console.log("animals", animals);
-
     setPets(animals || []);
+
+
   }
 
   useEffect(() => {
@@ -32,15 +35,17 @@ const SearchParams = () => {
     }, console.error);
   }, [animal]);
 
+  function submitHandler(e){
+    e.preventDefault()
+    requestPets()
+  }
+
   return (
+
     <div className="search-params">
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          requestPets();
-        }}
-      >
+      <form onSubmit={submitHandler}>
         <label htmlFor="location">
+        {console.log("qweq",pets)}
           Location
           <input
             id="location"
@@ -51,7 +56,19 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-        <button>Submit</button>
+        <label htmlFor="theme">
+          Theme 
+          <select value={theme} 
+          onChange={e=>setTheme(e.target.value)}
+          onBlur={e=>setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark blue</option>
+            <option value="mediumorchid">Medium Orchid</option>
+            <option value="Chartreuse">Chartreuse</option>
+          </select>
+        </label>
+        <button style={ {backgroundColor:theme}}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
